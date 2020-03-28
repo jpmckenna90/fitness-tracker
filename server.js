31553930e3aa342863529955df65000fbd06a5df
jpm <jpmckenna90@gmail.com>
2020-03-28 15:11:05 -0400

    Commented out seed line to see if that's causing app to crash

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 // const path = require("path");
-const seed = require("./Develop/seeders/seed.js");
+// const seed = require("./Develop/seeders/seed.js");
 
 const PORT = process.env.PORT || 3000;
 
